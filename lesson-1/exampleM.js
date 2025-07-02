86cf3d8e73295ba04b022264fc454422638c7ac6
Alessandra <alessandrag408@gmail.com>
2025-07-02 18:33:18 +0200

    example with Manfredi

---
+++ lesson-1/exampleM.js
@@ -0,0 +1,25 @@
+let firstName = 'Mario';
+
+firstName = 'Luigi'
+
+const lastName = 'Rossi';
+
+console.log(firstName);
+
+let age = 30;
+
+let admin = true;
+
+let city = null;
+
+let cap;
+
+const frutta = ['mela', 'pera', 'banana'];
+
+frutta[3] = 'kiwi'
+
+frutta[0] = 'arancia'
+
+console.log(frutta);
+
+console.log(frutta[10]);
